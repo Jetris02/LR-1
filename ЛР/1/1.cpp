571d75df925b3c86f3f1ba4d33f20f76926dc07c
Jetris02 <86570861+Jetris02@users.noreply.github.com>
2021-07-05 11:19:10 +0300

    Add files via upload

---
+++ ЛР/1/1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int N = 10;
+
+void set(int* a, int size)
+{
+	cout << "Введите элементы массива\n";
+	for (int i = 0; i < size; i++)
+		cin >> a[i];
+}
+
+void get(int* a, int size)
+{
+	cout << "\nМассив\n";
+	for (int i = 0; i < size; i++)
+		cout << a[i] << "  ";
+	cout << endl;
+}
+
+void change(int* a, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (a[i] % 2 == 0)
+			a[i] = 0;
+	}
+}
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	int array[N];
+	set(array, N);
+	get(array, N);
+	change(array, N);
+	get(array, N);
+	system("pause");
+}
---
+++ ЛР/2/2.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	char sym;
+	string str;
+	int sig = 1;
+	cout << "Введите символ\n";
+	cin >> sym;
+	while (sig)
+	{
+		if (isalpha(sym))
+			sig = 0;
+		else
+		{
+			cout << "Введите символ\n";
+			cin >> sym;
+		}
+	}
+	sig = 1;
+	cout << "Введите строку\n";
+	cin >> str;
+	int len;
+	len = str.length();
+	while (sig)
+	{
+		for (int i = 0; i < len; i++)
+		{
+			if (isalpha(str[i]))
+			{
+				if (i == len - 1)
+					sig = 0;
+			}
+			else
+			{
+				cout << "Введите строку\n";
+				cin >> str;
+				len = str.length();
+				break;
+			}
+		}
+	}
+	for (int i = 0; i < len; i++)
+	{
+		if (str[i] == sym)
+		{
+			cout << "Номер символа в строке - " << i + 1 << endl;
+			break;
+		}
+		if (i == len - 1)
+			cout << "Символ в строке отсутствует\n";
+	}
+	system("pause");
+}
